100e4e5ccf545eff0f38e27c8aca05ff10564101
Leon Andreasen <leon.andreasen@skola.botkyrka.se>
2020-12-07 00:25:49 +0100

    test

---
+++ loop-warmups.js
@@ -0,0 +1,18 @@
+function multiplied (arr){
+    let n = 1
+
+for (let i = 0; i < arr.length; i++) {
+    n = n * arr[i]
+    console.log(arr[i] + n)
+}
+return n
+}
+console.log(multiplied([4,4,5,8,]))
+
+function add(x){}
+while (6>x) {
+    let x = 0
+    x++
+    console.log(x)
+    return x
+}
